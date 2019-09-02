864d010536968e94c494474c446bef214d380dab
antoinel <antoinel@teck11.iut-infobio.priv.univ-lille1.fr>
2019-09-02 14:52:56 +0200

    coffre

---
+++ MedievalQuest/src/Structure/Coffre.java
@@ -0,0 +1,35 @@
+package Structure;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Coffre {
+	public static final Random R = new Random();
+	public ArrayList<String> stat = new ArrayList<>();
+	
+	public Coffre() {
+		this.stat.add("Vie");
+		this.stat.add("Atk");
+		this.stat.add("Def");
+		this.stat.add("$");
+	}
+	
+	public void ouvrirCoffre() {
+		if(this.bonus()) {
+			System.out.println("Vous gagnez 10 "+stat.get(R.nextInt(4)));
+		} else {
+			System.out.println("Vous perdez 5 "+stat.get(R.nextInt(4)));
+		}
+		System.out.println();
+		System.out.println("Appuyez sur <Entrée> pour continuer.");
+	}
+	
+	public boolean bonus() {
+		return R.nextInt(2)==1;
+	}
+	
+	public static void main(String[] args) {
+		Coffre c = new Coffre();
+		c.ouvrirCoffre();
+	}
+}
