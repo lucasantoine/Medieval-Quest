9919b18f697f28e3b6a9b7351e011471bf5a3f16
Geoffrey VANISCOTTE <vaniscog@teck18.iut-infobio.priv.univ-lille1.fr>
2019-09-02 14:45:53 +0200

    Ajout de la class Statistique

--- MedievalQuest/src/util/Statistique.java
+++ MedievalQuest/src/util/Statistique.java
@@ -1,3 +1,23 @@
 package util;
 public class Statistique {
+	private String unite;
+	private int val;
+	
+	public Statistique(String unite) {
+		this.unite = unite;
+		
+	}
+	
+	public void add(int n) {
+		val += n;
+	}
+	
+	public int getValue() {
+		return val;
+	}
+	
+	public String toString() {
+		return unite + " " + val;
+	}
+	
 }
