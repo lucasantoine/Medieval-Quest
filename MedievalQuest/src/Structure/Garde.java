5c705f1574d876b084e0adc3ce2662ffa065e56e
antoinel <antoinel@teck11.iut-infobio.priv.univ-lille1.fr>
2019-09-03 11:11:46 +0200

    garde

--- MedievalQuest/src/Structure/Garde.java
+++ MedievalQuest/src/Structure/Garde.java
@@ -31,34 +31,39 @@ public class Garde implements IEvenement {
 	}
 	
 	private static void seduire(Personnage p) {
-		System.out.println("Vous tentez de séduire le garde.");
+		Saisie.pressEnter("Vous tentez de séduire le garde.", false);
 		if(R.nextInt(2)==1) {
-			System.out.println("Il semble intéressé par vous et souhaite vous revoir ce soir. Il vous glisse un morceau de papier dans votre poche arrière et vous laisse passer.");	
+			Saisie.pressEnter("Il semble intéressé par vous et souhaite vous revoir ce soir. Il vous glisse un morceau de papier dans votre poche arrière et vous laisse passer.", false);	
 		} else {
-			System.out.println("Vous pensiez vraiment pouvoir séduire un garde ? Vexé, il vous frappe au visage et vous vole 5 pièces d'or.");
+			Saisie.pressEnter("Vous pensiez vraiment pouvoir séduire un garde ? Vexé, il vous frappe au visage et vous vole 5 pièces d'or.", false);
+			Saisie.pressEnter("Vous perdez 5 points de vie.", false);
 			p.getHud().getStats()[0].add(-5);
 			p.getHud().getStats()[3].add(-5);
 		}
 	}
 	
 	private static void payer(Personnage p) {
-		System.out.println("Vous tentez de payer le garde.");
+		Saisie.pressEnter("Vous tentez de payer le garde.", false);
 		if(p.getArg()>=20) {
-			System.out.println("Il accepte avec avidité et vous laisse poursuivre votre aventure.");
+			Saisie.pressEnter("Il accepte avec avidité et vous laisse poursuivre votre aventure.", false);
+			Saisie.pressEnter("Vous perdez 5$.", false);
 			p.getHud().getStats()[3].add(-5);
 		} else {
-			System.out.println("Il refuse, voyant votre bourse vide, et décide de se calmer en se défoulant sur vous.");
+			Saisie.pressEnter("Il refuse, voyant votre bourse vide, et décide de se calmer en se défoulant sur vous.", false);
+			Saisie.pressEnter("Vous perdez 5 points de vie.", false);
 			p.getHud().getStats()[0].add(-5);
 		}
 	}
 	
 	private static void intimider(Personnage p) {
-		System.out.println("Vous décidez de jouer des muscles afin d'intimider le garde.");
+		Saisie.pressEnter("Vous décidez de jouer des muscles afin d'intimider le garde.", false);
 		if(p.getAtk()>=20) {
-			System.out.println("Le garde, apeuré, s'enfuit en courant en laissant tomber sa bourse sur le sol.");
+			Saisie.pressEnter("Le garde, apeuré, s'enfuit en courant en laissant tomber sa bourse sur le sol.", false);
+			Saisie.pressEnter("Vous gagnez 10$.", false);
 			p.getHud().getStats()[3].add(10);
 		} else {
-			System.out.println("Le garde, nullement impressionné par votre numéro, vous met une baffe dont vous vous souviendrez toute votre vie.");
+			Saisie.pressEnter("Le garde, nullement impressionné par votre numéro, vous met une baffe dont vous vous souviendrez toute votre vie.", false);
+			Saisie.pressEnter("Vous perdez 5 points de vie.", false);
 			p.getHud().getStats()[0].add(-5);
 		}
 	}
@@ -66,8 +71,9 @@ public class Garde implements IEvenement {
 	
 	
 	private static void fuir(Personnage p) {
-		System.out.println("Vous prenez la fuite !");
-		System.out.println("Pensiez-vous vraiment pouvoir échapper à un garde ? Il vous rattrappe aisément et vous plante son épée dans la jambe.");
+		Saisie.pressEnter("Vous prenez la fuite !", false);
+		Saisie.pressEnter("Pensiez-vous vraiment pouvoir échapper à un garde ? Il vous rattrappe aisément et vous plante son épée dans la jambe.", false);
+		Saisie.pressEnter("Vous perdez 10 points de vie.", false);
 		p.getHud().getStats()[0].add(-10);
 	}
 }
