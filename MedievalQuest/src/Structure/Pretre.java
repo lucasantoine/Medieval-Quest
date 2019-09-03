ef6a841c3a28275668176423c7a76ce86377731f
antoinel <antoinel@teck11.iut-infobio.priv.univ-lille1.fr>
2019-09-03 11:37:16 +0200

    v

--- MedievalQuest/src/Structure/Pretre.java
+++ MedievalQuest/src/Structure/Pretre.java
@@ -1,8 +1,33 @@
 package Structure;
 
+import interaction.Saisie;
 import util.IEvenement;
 
 public class Pretre implements IEvenement {
 	
-
+	public static void pretre(Personnage p) {
+		System.out.println("Vous entrez dans une église où un prêtre vous propose de recevoir la bénédiction du Seigneur");
+		String s = Saisie.saisie("Que choisissez vous ? (1 - Vie, 2 - Attaque, 3 - Défense, 4 - Argent, 5 - Partir) : \n", 5);
+		int choix = Saisie.stringToInt(s);
+		Saisie.clearScreenOpti();
+		Pretre.getChoix(choix, p);
+	}
+	
+	private static void getChoix(int choix, Personnage p) {
+		switch(choix) {
+		case 1 : ; break;
+		case 2 : ; break;
+		case 3 : ; break;
+		case 4 : ; break;
+		case 5 : ; break;
+		}
+	}
+	
+	private static void atk(Personnage p) {
+		
+	}
+	
+	private static void def(Personnage p) {
+		
+	}
 }
