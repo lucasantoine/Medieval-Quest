2121fe440e890f613716f5f5560557a55864ce2e
antoinel <antoinel@teck11.iut-infobio.priv.univ-lille1.fr>
2019-09-03 16:24:14 +0200

    ascii

--- MedievalQuest/src/menu/Context.java
+++ MedievalQuest/src/menu/Context.java
@@ -4,15 +4,15 @@ import interaction.Saisie;
 
 public class Context {
 	public static void showContext() {
-		Saisie.pressEnter("Des qu'un message vous sera affiché vous pourrez le passer en appuyant sur ENTER et ce durant toute votre épopé.", true);
-		Saisie.pressEnter("Bienvenu à \"Medieval Kingdome\" !", false);
+		Saisie.pressEnter("Dès qu'un message vous sera affiché vous pourrez le passer en appuyant sur ENTER et ce durant toute votre épopée.", true);
+		Saisie.pressEnter("Bienvenue à \"Medieval Kingdom\" !", false);
 		
-		Saisie.pressEnter("Ce royaume verdoyant est l'un des seul à vivre en paix.", false);
-		Saisie.pressEnter("Mais tout lumière à sa part d'ombre...", false);
+		Saisie.pressEnter("Ce royaume verdoyant est l'un des seul à vivre encore en paix.", false);
+		Saisie.pressEnter("Mais toute lumière à sa part d'ombre...", false);
 		Saisie.pressEnter("Cela fait 10 ans que tous les villageois vivent dans la peur.", false);
-		Saisie.pressEnter("\"Tartanpion\" ce nom remplie de terreur tout Homme des environs.", false);
-		Saisie.pressEnter("Nulle ne sait d'ou il vient mais une aura mystique entour se personnage craint de tous.", false);
-		Saisie.pressEnter("Je vous sais néanmoins assez courageux pour gravir les échellons.", false);
+		Saisie.pressEnter("\"Tartanpion\" ce nom rempli de terreur tout Homme des environs.", false);
+		Saisie.pressEnter("Nul ne sait d'où il vient mais une aura mystique entoure ce personnage craint de tous.", false);
+		Saisie.pressEnter("Je vous sais néanmoins assez courageux pour gravir les échelons.", false);
 		Saisie.pressEnter("Je vous sais assez pur pour rétablir l'ordre dans ce Royaume.", false);
 		Saisie.pressEnter("Je vous sais capable d'aller au bout... du MEDIEVAL QUEST !", false);
 	}
