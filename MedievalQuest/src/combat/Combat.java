8774d0fbd30556ee68b2d788f846745e62c25381
Geoffrey VANISCOTTE <vaniscog@teck18.iut-infobio.priv.univ-lille1.fr>
2019-09-03 13:46:02 +0200

    Fix des affichage des boss

--- MedievalQuest/src/combat/Combat.java
+++ MedievalQuest/src/combat/Combat.java
@@ -11,24 +11,21 @@ public class Combat {
 	
 	public static void startCombat(Personnage player, Personnage ennemi) {
 		Random r = new Random();
+		System.out.println("Joueur :\n"+player.getHud().toString()+"\nEnnemi :\n"+ennemi.getHud().toString());
+		String s = Saisie.saisie("Que decidez vous de faire ( 1 - Attaquer, 2 - Defendre) : \n", 2);
+		int choix = Saisie.stringToInt(s);
+		if(choix == 1) player.setAttaqueBool(true);
+		if(choix == 2) player.setAttaqueBool(false);
 		
-		while(player.getVie() > 0 && ennemi.getVie() > 0) {
-			System.out.println("Joueur :\n"+player.getHud().toString()+"\nEnnemi :\n"+ennemi.getHud().toString());
-			String s = Saisie.saisie("Que decidez vous de faire ( 1 - Attaquer, 2 - Defendre) : \n", 2);
-			int choix = Saisie.stringToInt(s);
-			if(choix == 1) player.setAttaqueBool(true);
-			if(choix == 2) player.setAttaqueBool(false);
-			
-			if(r.nextInt(2) == 0) {
-				ennemi.setAttaqueBool(true);
-			}else {
-				ennemi.setAttaqueBool(false);
-			}
-			
-			if(player.isAttaque()) ennemi.doEffet(new Effet(EStat.VIE, player.getAtk()));
-			if(ennemi.isAttaque()) player.doEffet(new Effet(EStat.VIE, ennemi.getAtk()));
-			System.out.println(player.getVie() + "   " + ennemi.getVie());
-			Saisie.clearScreenOpti();
+		if(r.nextInt(2) == 0) {
+			ennemi.setAttaqueBool(true);
+		}else {
+			ennemi.setAttaqueBool(false);
 		}
+		
+		if(player.isAttaque()) ennemi.doEffet(new Effet(EStat.VIE, player.getAtk()));
+		if(ennemi.isAttaque()) player.doEffet(new Effet(EStat.VIE, ennemi.getAtk()));
+		System.out.println(player.getVie() + "   " + ennemi.getVie());
+		Saisie.clearScreenOpti();
 	}
 }
