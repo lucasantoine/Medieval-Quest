882c281dbea308ccda04ee45ee099150348432c1
Virgil <virgil031299@gmail.com>
2019-09-03 14:11:32 +0200

    Suppr de TestAdrien ajout de la fonction qui affiche le Contexte

---
+++ MedievalQuest/src/menu/Context.java
@@ -0,0 +1,19 @@
+package menu;
+
+import interaction.Saisie;
+
+public class Context {
+	public static void showContext() {
+		Saisie.pressEnter("Des qu'un message vous sera affiché vous pourrez le passer en appuyant sur ENTER et ce durant toute votre épopé.", true);
+		Saisie.pressEnter("Bienvenu à \"Medieval Kingdome\" !", false);
+		
+		Saisie.pressEnter("Ce royaume verdoyant est l'un des seul à vivre en paix.", false);
+		Saisie.pressEnter("Mais tout lumière à sa part d'ombre...", false);
+		Saisie.pressEnter("Cela fait 10 ans que tous les villageois vivent dans la peur.", false);
+		Saisie.pressEnter("\"Tartanpion\" ce nom remplie de terreur tout Homme des environs.", false);
+		Saisie.pressEnter("Nulle ne sait d'ou il vient mais une aura mystique entour se personnage craint de tous.", false);
+		Saisie.pressEnter("Je vous sais néanmoins assez courageux pour gravir les échellons.", false);
+		Saisie.pressEnter("Je vous sais assez pur pour rétablir l'ordre dans ce Royaume.", false);
+		Saisie.pressEnter("Je vous sais capable d'aller au bout... du MEDIEVAL QUEST !", false);
+	}
+}
