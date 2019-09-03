b16c7791ba0b517241c67ae5b100af357f416491
Geoffrey VANISCOTTE <vaniscog@teck18.iut-infobio.priv.univ-lille1.fr>
2019-09-03 10:24:45 +0200

    Ajout de l'interface IEvenemen

---
+++ MedievalQuest/src/util/IEvenement.java
@@ -0,0 +1,10 @@
+package util;
+
+import Structure.Personnage;
+
+public interface IEvenement {
+	/**
+	 * Methode permettant le lancement de l'evenement
+	 */
+	public void start(Personnage player);
+}
